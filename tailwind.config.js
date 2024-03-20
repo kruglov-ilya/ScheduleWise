fc92e1ee1a3494440d70086e5a7ea9b4d6b7e3dd
kruglov-ilya <kruglov-il1@yandex.ru>
2024-03-21 00:08:56 +0400

    feat: setting encore

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.{js,ts}",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
