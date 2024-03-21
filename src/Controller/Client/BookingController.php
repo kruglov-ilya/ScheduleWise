16c091865ab406a8c5a96cf80376b0571f2f7d5f
kruglov-ilya <kruglov-il1@yandex.ru>
2024-03-21 05:29:31 +0400

    feat: add relation profile to booking

--- src/Controller/Client/BookingController.php
+++ src/Controller/Client/BookingController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Client;
 use App\Entity\Booking;
 use App\Entity\Service;
 use App\Entity\Timeslot;
+use App\Entity\User;
 use App\Form\BookingFormType;
 use App\Repository\BookingRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,7 +18,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class BookingController extends AbstractController
 {
     #[Route('/booking', name: 'app_booking')]
-    public function index(Request $request, BookingRepository $repository, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, BookingRepository $repository, EntityManagerInterface $entityManager, Security $security): Response
     {
         $booking = new Booking();
         $form = $this->createForm(BookingFormType::class, $booking);
@@ -24,6 +26,13 @@ class BookingController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             if ($entityManager->find(Timeslot::class, $form->get('timeslot')->getData())->getCount() - 1 >= $repository->count(['timeslot' => $booking->getTimeslot()])){
+                /**
+                 * @var User $user
+                 */
+                if ($user = $security->getUser()) {
+                    $booking->setClient($user);
+                }
+
                 $booking->setPrice($entityManager->find(Service::class, $form->get('service')->getData())->getPrice());
                 $entityManager->persist($booking);
                 $entityManager->flush();
